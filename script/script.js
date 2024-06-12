079ec49292c44fd77ed1496bffd92514ee29845d
kimnani01 <165613415+kimnani01@users.noreply.github.com>
2024-06-12 21:06:39 +0900

    swiper js, css 1차 완료

---
+++ script/script.js
@@ -0,0 +1,37 @@
+// visual slide
+const visual = new Swiper('.visual', {
+        autoplay:{delay:3000},
+        loop: true,
+        navigation:{
+            nextEl:'.visual_wrap .swiper-button-next',
+            prevEl:'.visual_wrap .swiper-button-prev',
+        }
+    }
+)
+// md pick slide
+const pick = new Swiper('.pick', {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        spaceBetween: 10,
+        autoplay:{delay:3000},
+        loop: true,
+        pagination:{
+            el:'.pick .swiper-pagination', //부모 필수
+            type:'bullets',
+            clickable: true,
+        }   
+    }    
+)
+// 신상품
+const new_slide = new Swiper('.new_slide', {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 30,
+        autoplay:{delay:3000},
+        loop: true,
+        pagination:{
+            el:'.new_slide .swiper-pagination', //부모 필수
+            type:'bullets',
+            clickable: true,
+        }   
+})
